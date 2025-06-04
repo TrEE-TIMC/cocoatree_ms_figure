cb6cade8c54e7c83f14f444ff3160d21bdf263d1
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2025-06-04 16:45:51 +0200

    Figure 3

--- scripts/cocoatree_with_groundtruth/figure3.py
+++ scripts/cocoatree_with_groundtruth/figure3.py
@@ -1,67 +1,132 @@
 import matplotlib.pyplot as plt
+import itertools
+import numpy as np
 import pandas as pd
-from joblib import Memory
-from cocoatree import datasets
-from cocoatree.msa import filter_sequences
-from cocoatree.statistics.position import compute_conservation
-from cocoatree.statistics import pairwise
+from plotmastery.utils_subfigure import add_letter_and_title
+from plotmastery import utils_heatmap
 
-mem = Memory(".joblib")
-
-dataset = "rivoire"
-serine_dataset = datasets.load_S1A_serine_proteases(paper=dataset)
-seq_id = serine_dataset["sequence_ids"]
-sequences = serine_dataset["alignment"]
-n_pos, n_seq = len(sequences[0]), len(sequences)
-
-seq_kept, seq_id_kept, pos_kept = filter_sequences(sequences, seq_id)
+dataset = "rhomboid"
 
 # Load precomputed results
-res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
-res = res.loc[~res["filtered_msa_pos"].isna()] 
+sca_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
+sca_res = sca_res.loc[~sca_res["filtered_msa_pos"].isna()]
 
-sector_cols = [col for col in res.columns if col.startswith("sector")]
-is_in_sectors = res[sector_cols].sum(axis=1) > 0
+mi_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_MI_none.csv")
+mi_res = mi_res.loc[~mi_res["filtered_msa_pos"].isna()]
 
-###############################################################################
-# Compute matrices and conservation
+nmi_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_NMI_none.csv")
+nmi_res = nmi_res.loc[~nmi_res["filtered_msa_pos"].isna()]
 
-conservation = mem.cache(compute_conservation)(seq_kept)
-sca_matrix = mem.cache(pairwise.compute_sca_matrix)(seq_kept)
-mi_matrix = mem.cache(pairwise.compute_mutual_information_matrix)(
-    seq_kept,
-    normalize=False)
-nmi_matrix = mem.cache(pairwise.compute_mutual_information_matrix)(seq_kept)
-_, mi_apc_matrix = mem.cache(pairwise.compute_apc)(mi_matrix)
-
-###############################################################################
-# plot results
-fig, axes = plt.subplots(figsize=(8, 4), nrows=2, ncols=4)
-
-ax = axes[0, 0]
-
-def plot_conservation_vs_matrix(ax, matrix, colors):
-    ax.scatter(conservation, matrix.sum(axis=0),
-               c=colors,
-               marker=".", linewidth=0)
-    ax.spines["right"].set_linewidth(0)
-    ax.spines["top"].set_linewidth(0)
+miapc_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_MI_APC.csv")
+miapc_res = miapc_res.loc[~miapc_res["filtered_msa_pos"].isna()]
 
 
-plot_conservation_vs_matrix(axes[0, 0], sca_matrix, sca_matrix.sum(axis=1))
-plot_conservation_vs_matrix(axes[0, 1], mi_matrix, sca_matrix.sum(axis=1))
-plot_conservation_vs_matrix(axes[0, 2], nmi_matrix, sca_matrix.sum(axis=1))
-plot_conservation_vs_matrix(axes[0, 3], mi_apc_matrix, sca_matrix.sum(axis=1))
+sector_cols = [c for c in sca_res.columns if c.startswith("sector")]
+orig_sector_cols = [c for c in sca_res.columns if c.startswith("orig_sector")]
 
-ax = axes[1, 1]
-ax.plot(sca_matrix.flatten(), mi_matrix.flatten(), linewidth=0, marker=".",
-color="0")
-ax.spines["right"].set_linewidth(0)
-ax.spines["top"].set_linewidth(0)
+# Get original sectors in the original order
+orig_sectors = [np.where(sca_res[c])[0] for c in orig_sector_cols]
 
-ax = axes[1, 2]
-ax.plot(sca_matrix.flatten(), nmi_matrix.flatten(), linewidth=0, marker=".",
-color="0")
-ax.spines["right"].set_linewidth(0)
-ax.spines["top"].set_linewidth(0)
 
+# Now create some form of confusion matrix
+def compute_IOU_metric(set1, set2, metric="intersection"):
+    union = len(set(set1).union(set2))
+    intersection = len(set(set1).intersection(set2))
+    if metric == "IOU":
+        return intersection / union
+    else:
+        return intersection
+
+
+def compute_all_vs_all(sectors_per_method, comp1=0, comp2=0):
+    IOU_metric = np.zeros(
+        (len(sectors_per_method), len(sectors_per_method)))
+    for i, met1 in enumerate(sectors_per_method):
+        for j, met2 in enumerate(sectors_per_method):
+            IOU_metric[i, j] = compute_IOU_metric(met1[comp1], met2[comp2])
+    return IOU_metric
+
+
+def get_best_ordered_sectors(res):
+    sector_cols = [c for c in res.columns if c.startswith("sector")]
+
+    sectors = []
+    # It's not the best strategy, but just get iteratively the "best
+    # sector"
+    for s in orig_sectors:
+        all_scores = []
+        for s1 in sector_cols:
+            all_scores.append(
+                compute_IOU_metric(
+                    s,
+                    np.where(res[s1])[0]))
+
+        s = sector_cols.pop(np.argmax(all_scores))
+        sectors.append(np.where(res[s])[0])
+    return sectors
+
+
+sca_sectors = get_best_ordered_sectors(sca_res)
+mi_sectors = get_best_ordered_sectors(mi_res)
+nmi_sectors = get_best_ordered_sectors(nmi_res)
+miapc_sectors = get_best_ordered_sectors(miapc_res)
+all_sectors = [orig_sectors, sca_sectors, mi_sectors, nmi_sectors,
+               miapc_sectors]
+
+n_comp = len(sca_sectors)
+
+letters = ["A", "B", "C"]
+sector_names = ["Red", "Blue", "Green"]
+
+fig, axes = plt.subplots(figsize=(8, 8), ncols=n_comp, nrows=n_comp)
+for i, j in itertools.product(range(3), range(3)):
+
+    ax = axes[i, j]
+    if i > j:
+        ax.set_axis_off()
+        continue
+
+    IOU_metric_comp1 = compute_all_vs_all(all_sectors, comp1=i, comp2=j)
+    m = ax.matshow(IOU_metric_comp1, vmin=0, cmap="Oranges")
+    utils_heatmap.annotate_heatmap(
+        m, valfmt="{x:1.0f}",
+        fontsize="x-small")
+    if i == 0:
+        ax.set_xticklabels(
+            ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
+            fontsize="x-small")
+        ax.set_title(sector_names[j], fontweight="bold", fontsize="medium")
+    else:
+        ax.set_xticks([])
+    if i == j:
+        ax.set_yticklabels(
+            ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
+            fontsize="x-small")
+        ax.set_ylabel(sector_names[j], fontweight="bold")
+    else:
+        ax.set_yticks([])
+
+
+cmaps = ["Reds", "Blues", "Greens"]
+fig, axes = plt.subplots(figsize=(8, 3), ncols=n_comp,
+                         tight_layout=True)
+for i in range(n_comp):
+
+    ax = axes[i]
+    IOU_metric_comp1 = compute_all_vs_all(all_sectors, comp1=i,
+    comp2=i)
+    m = ax.matshow(IOU_metric_comp1, vmin=0, cmap=cmaps[i])
+    utils_heatmap.annotate_heatmap(
+        m, valfmt="{x:1.0f}",
+        fontsize="x-small")
+    ax.set_xticklabels(
+            ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
+            fontsize="x-small")
+    ax.set_yticklabels(
+            ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
+            fontsize="x-small")
+    ax.tick_params(axis='both', which='both', labelsize='x-small',
+               bottom=True, top=False, labeltop=False, labelbottom=True,
+               left=True, right=False, labelright=False, labelleft=True)
+
+    add_letter_and_title(ax, letters[i], sector_names[i])
