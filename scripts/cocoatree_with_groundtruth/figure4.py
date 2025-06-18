ca3d96d8675e87a62170b7e62a35fabe59f30f6d
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2025-06-18 21:45:25 +0200

    Let's make the plot slightly better for figure 4

--- scripts/cocoatree_with_groundtruth/figure4.py
+++ scripts/cocoatree_with_groundtruth/figure4.py
@@ -5,20 +5,24 @@ from cocoatree import datasets
 from cocoatree.msa import filter_sequences
 from cocoatree.statistics.position import compute_conservation
 from cocoatree.statistics import pairwise
+from plotmastery.utils_subfigure import add_letter_and_title
 
 mem = Memory(".joblib")
 
-dataset = "halabi"
-serine_dataset = datasets.load_S1A_serine_proteases(paper=dataset)
-seq_id = serine_dataset["sequence_ids"]
-sequences = serine_dataset["alignment"]
+dataset = "rhomboid"
+if dataset in ["rivoire", "halabi"]:
+    data = datasets.load_S1A_serine_proteases(paper=dataset)
+else:
+    data = datasets.load_rhomboid_proteases()
+seq_id = data["sequence_ids"]
+sequences = data["alignment"]
 n_pos, n_seq = len(sequences[0]), len(sequences)
 
 seq_kept, seq_id_kept, pos_kept = filter_sequences(sequences, seq_id)
 
 # Load precomputed results
 res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
-res = res.loc[~res["filtered_msa_pos"].isna()] 
+res = res.loc[~res["filtered_msa_pos"].isna()]
 
 sector_cols = [col for col in res.columns if col.startswith("sector")]
 is_in_sectors = res[sector_cols].sum(axis=1) > 0
@@ -39,7 +43,6 @@ _, mi_apc_matrix = mem.cache(pairwise.compute_apc)(mi_matrix)
 fig, axes = plt.subplots(figsize=(8, 2), nrows=1, ncols=4, squeeze=False,
 tight_layout=True)
 
-ax = axes[0, 0]
 
 def plot_conservation_vs_matrix(ax, matrix, colors):
     ax.scatter(conservation, matrix.sum(axis=0),
@@ -47,10 +50,24 @@ def plot_conservation_vs_matrix(ax, matrix, colors):
                marker=".", linewidth=0)
     ax.spines["right"].set_linewidth(0)
     ax.spines["top"].set_linewidth(0)
-    ax.set_xlabel("Conservation", fontweight="bold", fontsize="small")
-
+    ax.set_xlabel("Conservation", fontweight="bold", fontsize="small",
+                  labelpad=2)
+    ax.tick_params(
+        axis='both', which='both', labelsize='x-small',
+        bottom=True, top=False, labeltop=False, labelbottom=True,
+        left=True, right=False, labelleft=True, labelright=False)
 
 plot_conservation_vs_matrix(axes[0, 0], sca_matrix, sca_matrix.sum(axis=1))
+axes[0, 0].set_ylabel("Cum score", fontsize="small", fontweight="bold",
+                      labelpad=2)
+add_letter_and_title(axes[0, 0], "A", "SCA")
+
 plot_conservation_vs_matrix(axes[0, 1], mi_matrix, sca_matrix.sum(axis=1))
+add_letter_and_title(axes[0, 1], "B", "MI")
+
 plot_conservation_vs_matrix(axes[0, 2], nmi_matrix, sca_matrix.sum(axis=1))
+add_letter_and_title(axes[0, 2], "B", "NMI")
+
 plot_conservation_vs_matrix(axes[0, 3], mi_apc_matrix, sca_matrix.sum(axis=1))
+add_letter_and_title(axes[0, 3], "A", "MI+APC")
+fig.savefig(f"figures/{dataset}_conservation.pdf")
