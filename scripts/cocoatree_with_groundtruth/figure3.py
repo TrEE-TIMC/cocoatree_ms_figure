b45c262aa9521acc83ddf59057658901ef2a5517
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2025-06-24 16:17:08 +0200

    Let's revert back to plotting only confusion matrices for one dataset

--- scripts/cocoatree_with_groundtruth/figure3.py
+++ scripts/cocoatree_with_groundtruth/figure3.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
-import itertools
 import numpy as np
 import pandas as pd
 from utils.vis import sectors_cm
-from utils.sectors import get_best_ordered_sectors
 from plotmastery.utils_subfigure import add_letter_and_title
 from plotmastery import utils_heatmap
 from utils.sectors import compute_IOU_metric
 
-datasets = ["halabi", "rhomboid", "DHFR"]
+datasets = ["halabi"]
+
 
 def compute_all_vs_all(sectors_per_method, comp1=0, comp2=0):
     IOU_metric = np.zeros(
@@ -21,28 +20,34 @@ def compute_all_vs_all(sectors_per_method, comp1=0, comp2=0):
 
 cmaps = list(sectors_cm.values())
 
-fig  = plt.figure(figsize=(7.5, 7.5), 
-                  tight_layout=True)
+fig = plt.figure(figsize=(7.5, 3),
+                 tight_layout=True)
 axes = []
 for j, dataset in enumerate(datasets):
     dataset_ax = []
     # Load precomputed results
-    sca_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
+    sca_res = pd.read_csv(
+        f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
     sca_res = sca_res.loc[~sca_res["filtered_msa_pos"].isna()]
 
-    mi_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_MI_none.csv")
+    mi_res = pd.read_csv(
+        f"results/cocoatree_gt/{dataset}/cocoatree_MI_none.csv")
     mi_res = mi_res.loc[~mi_res["filtered_msa_pos"].isna()]
 
-    nmi_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_NMI_none.csv")
+    nmi_res = pd.read_csv(
+        f"results/cocoatree_gt/{dataset}/cocoatree_NMI_none.csv")
     nmi_res = nmi_res.loc[~nmi_res["filtered_msa_pos"].isna()]
 
-    miapc_res = pd.read_csv(f"results/cocoatree_gt/{dataset}/cocoatree_MI_APC.csv")
+    miapc_res = pd.read_csv(
+        f"results/cocoatree_gt/{dataset}/cocoatree_MI_APC.csv")
     miapc_res = miapc_res.loc[~miapc_res["filtered_msa_pos"].isna()]
 
-
-    sector_cols = [c for c in sca_res.columns if c.startswith("sector")]
+    sector_cols = [
+        c for c in sca_res.columns if c.startswith("sector")]
     sector_cols.sort()
-    orig_sector_cols = [c for c in sca_res.columns if c.startswith("orig_sector")]
+
+    orig_sector_cols = [
+        c for c in sca_res.columns if c.startswith("orig_sector")]
 
     # Get original sectors in the original order
     orig_sectors = [np.where(sca_res[c])[0] for c in orig_sector_cols]
@@ -51,15 +56,13 @@ for j, dataset in enumerate(datasets):
     nmi_sectors = [np.where(nmi_res[c])[0] for c in sector_cols]
     miapc_sectors = [np.where(miapc_res[c])[0] for c in sector_cols]
 
-
     all_sectors = [orig_sectors, sca_sectors, mi_sectors, nmi_sectors,
-                miapc_sectors]
+                   miapc_sectors]
 
     n_comp = len(sca_sectors)
 
-    letters = ["A", "B", "C", "D"]
+    letters = ["A.", "B.", "C.", "D."]
     sector_names = ["Green", "Red", "Blue", "Purple"]
-
 
     for i in range(n_comp):
 
@@ -70,26 +73,32 @@ for j, dataset in enumerate(datasets):
         utils_heatmap.annotate_heatmap(
             m, valfmt="{x:1.0f}",
             fontsize="x-small")
-        if i == 0:
-            ax.set_yticklabels(
-                    ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
-                    fontsize="x-small")
-        else:
-            ax.set_yticklabels([])
+        ax.set_yticklabels(
+                ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
+                fontsize="x-small")
+        ax.set_xticklabels(
+                ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
+                fontsize="x-small", rotation=90)
+        ax.tick_params(
+            axis='both', which='both', labelsize='x-small',
+            bottom=True, top=False, labeltop=False, labelbottom=True,
+            left=True, right=False, labelright=False, labelleft=True)
 
-        if j == 2:
-            ax.set_xticklabels(
-                    ["", "ori.", "SCA", "MI", "NMI", "MI+APC"],
-                    fontsize="x-small", rotation=90)
-        else:
-            ax.set_xticklabels([])
-        ax.tick_params(axis='both', which='both', labelsize='x-small',
-                bottom=True, top=False, labeltop=False, labelbottom=True,
-                left=True, right=False, labelright=False, labelleft=True)
     axes.append(dataset_ax)
 
-add_letter_and_title(axes[0][0], letters[0], "Serine protease")
-add_letter_and_title(axes[1][0], letters[1], "Rhomboid")
-add_letter_and_title(axes[2][0], letters[2], "DHFR")
+add_letter_and_title(
+    axes[0][0],
+    letters[0],
+    "Green sector")
 
-fig.savefig(f"figures/figure_4.pdf")
+add_letter_and_title(
+    axes[0][1],
+    letters[1],
+    "Red sector")
+
+add_letter_and_title(
+    axes[0][2],
+    letters[2],
+    "Blue sector")
+
+fig.savefig("figures/figure_4.pdf")
