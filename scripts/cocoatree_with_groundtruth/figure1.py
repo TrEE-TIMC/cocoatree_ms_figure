c49a6d64f34fcca3ccf86742baa960a8b6fad736
margauxjullien <margauxjullien3@gmail.com>
2025-10-31 16:07:27 +0100

    exchange order of Rhomboid and DHFR

--- scripts/cocoatree_with_groundtruth/figure1.py
+++ scripts/cocoatree_with_groundtruth/figure1.py
@@ -28,20 +28,6 @@ add_letter_and_title(ax, "A.", "Serine protease")
 
 
 ###############################################################################
-# rhomboid results
-results = pd.read_csv("results/cocoatree_gt/rhomboid/cocoatree_SCA_none.csv")
-results = annotate_results(results)
-
-ax = fig.add_subplot(gs[:12, 10:18])
-plot_scatter_sectors(ax, results, "IC1", "IC2", annotate=False,
-                     add_labels=False)
-ax.set_ylabel("Green", fontweight="bold", fontsize="small", labelpad=2)
-ax.set_xlabel("Red", fontweight="bold", fontsize="small", labelpad=2)
-
-add_letter_and_title(ax, "B.", "Rhomboid")
-
-
-###############################################################################
 # DHFR results
 results = pd.read_csv("results/cocoatree_gt/DHFR/cocoatree_SCA_none.csv")
 results = annotate_results(results)
@@ -53,7 +39,21 @@ plot_scatter_sectors(
 ax.set_ylabel("Green", fontweight="bold", fontsize="small", labelpad=2)
 ax.set_xlabel("Red", fontweight="bold", fontsize="small", labelpad=2)
 
-add_letter_and_title(ax, "C.", "DHFR")
+add_letter_and_title(ax, "B.", "DHFR")
+
+
+###############################################################################
+# rhomboid results
+results = pd.read_csv("results/cocoatree_gt/rhomboid/cocoatree_SCA_none.csv")
+results = annotate_results(results)
+
+ax = fig.add_subplot(gs[:12, 10:18])
+plot_scatter_sectors(ax, results, "IC1", "IC2", annotate=False,
+                     add_labels=False)
+ax.set_ylabel("Green", fontweight="bold", fontsize="small", labelpad=2)
+ax.set_xlabel("Red", fontweight="bold", fontsize="small", labelpad=2)
+
+add_letter_and_title(ax, "C.", "Rhomboid")
 
 
 ###############################################################################
@@ -109,38 +109,6 @@ ax.xaxis.set_major_locator(plt.MaxNLocator(5))
 
 start_i += shift
 
-dataset = "rhomboid"
-results = pd.read_csv(
-    f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
-results = annotate_results(results)
-results = results.loc[~results["pdb_pos"].isna()]
-
-columns = [col for col in results.columns if col.startswith("orig_sector")]
-order = [col for col in results.columns if col.startswith("sector")]
-# Cocoatree sectors
-ax = fig.add_subplot(gs[start_i, :-2])
-add_letter_and_title(ax, "E.", title="Rhomboid")
-plot_sectors(ax, results, columns=order, title="cocoatree")
-
-# Original sectors
-ax = fig.add_subplot(gs[start_i+1, :-2])
-plot_sectors(ax, results,
-             columns=columns,
-             title="orig")
-
-# Both
-ax = fig.add_subplot(gs[start_i+2, :-2])
-diff = results["is_both"].values[np.newaxis, :].astype(float)
-ax.matshow(diff, aspect="auto", cmap="Greys")
-ax.set_yticks([0])
-ax.set_yticklabels(["both"], fontweight="bold")
-ax.tick_params(axis='both', which='both', labelsize='x-small',
-               bottom=True, top=False, labeltop=False, labelbottom=True,
-               left=False, right=False, labelleft=False, labelright=True)
-ax.xaxis.set_major_locator(plt.MaxNLocator(5))
-
-start_i += shift
-
 dataset = "DHFR"
 results = pd.read_csv(
     f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
@@ -151,7 +119,7 @@ columns = [col for col in results.columns if col.startswith("orig_sector")]
 order = [col for col in results.columns if col.startswith("sector")]
 # Cocoatree sectors
 ax = fig.add_subplot(gs[start_i, :-2])
-add_letter_and_title(ax, "F.", title="DHFR")
+add_letter_and_title(ax, "E.", title="DHFR")
 plot_sectors(ax, results, columns=order, title="cocoatree")
 
 # Original sectors
@@ -172,6 +140,38 @@ ax.tick_params(axis='both', which='both', labelsize='x-small',
 ax.xaxis.set_major_locator(plt.MaxNLocator(5))
 ax.set_xlabel("Position in PDB", fontweight="bold", fontsize="small",
               labelpad=2)
+
+start_i += shift
+
+dataset = "rhomboid"
+results = pd.read_csv(
+    f"results/cocoatree_gt/{dataset}/cocoatree_SCA_none.csv")
+results = annotate_results(results)
+results = results.loc[~results["pdb_pos"].isna()]
+
+columns = [col for col in results.columns if col.startswith("orig_sector")]
+order = [col for col in results.columns if col.startswith("sector")]
+# Cocoatree sectors
+ax = fig.add_subplot(gs[start_i, :-2])
+add_letter_and_title(ax, "F.", title="Rhomboid")
+plot_sectors(ax, results, columns=order, title="cocoatree")
+
+# Original sectors
+ax = fig.add_subplot(gs[start_i+1, :-2])
+plot_sectors(ax, results,
+             columns=columns,
+             title="orig")
+
+# Both
+ax = fig.add_subplot(gs[start_i+2, :-2])
+diff = results["is_both"].values[np.newaxis, :].astype(float)
+ax.matshow(diff, aspect="auto", cmap="Greys")
+ax.set_yticks([0])
+ax.set_yticklabels(["both"], fontweight="bold")
+ax.tick_params(axis='both', which='both', labelsize='x-small',
+               bottom=True, top=False, labeltop=False, labelbottom=True,
+               left=False, right=False, labelleft=False, labelright=True)
+ax.xaxis.set_major_locator(plt.MaxNLocator(5))
 
 
 os.makedirs("figures", exist_ok=True)
