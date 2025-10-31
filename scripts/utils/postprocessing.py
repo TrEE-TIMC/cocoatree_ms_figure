7704f7e87a58a770aef601b7afdf7f46816aaf8f
margauxjullien <margauxjullien3@gmail.com>
2025-10-31 16:25:50 +0100

    replace sector by xcor

--- scripts/utils/postprocessing.py
+++ scripts/utils/postprocessing.py
@@ -5,10 +5,10 @@ def annotate_results(results):
     warnings.simplefilter("ignore")
     results = results.loc[~results["filtered_msa_pos"].isna()]
 
-    sca_sectors = [c for c in results.columns if c.startswith("sector")]
+    sca_xcors = [c for c in results.columns if c.startswith("xcor")]
     orig_sectors = [c for c in results.columns if c.startswith("orig_")]
 
-    results.loc[:, "is_cocoatree_sector"] = results[sca_sectors].sum(
+    results.loc[:, "is_cocoatree_sector"] = results[sca_xcors].sum(
         axis=1) > 0
 
     results.loc[:, "is_orig_sector"] = results[orig_sectors].sum(
