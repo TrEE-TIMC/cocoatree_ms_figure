36a72523b3e449ae6451b5ddcd70e5cc1cef70e3
margauxjullien <margauxjullien3@gmail.com>
2026-01-27 10:33:27 +0100

    clean up script

--- scripts/cocoatree_with_groundtruth/plot_trypsin_vs_chymotrypsin.py
+++ scripts/cocoatree_with_groundtruth/plot_trypsin_vs_chymotrypsin.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from plotmastery.utils_subfigure import add_letter_and_title
-
 from cocoatree.datasets import load_S1A_serine_proteases
 from cocoatree.io import load_MSA
 from cocoatree.msa import filter_seq_id, compute_seq_identity
@@ -11,12 +10,12 @@ halabi = load_S1A_serine_proteases('halabi')
 halabi_seq = halabi['alignment']
 halabi_id = halabi['sequence_ids']
 
-sector_file = '/home/jullimar/Documents/Postdoc_TIMC/2023-margaux-cocoatree/scripts/cocoatree_with_groundtruth/results/cocoatree_gt/halabi/cocoatree_xcor_1_SCA_none.fasta'
+sector_file = 'results/cocoatree_gt/halabi/cocoatree_xcor_1_SCA_none.fasta'
 sector = load_MSA(sector_file, 'fasta')
 sector_seq = sector['alignment']
 sector_id = sector['sequence_ids']
 
-metadata = '/home/jullimar/Documents/Postdoc_TIMC/2023-margaux-cocoatree/data/Trypsin/Halabi/halabi_metadata.csv'
+metadata = '../../data/Trypsin/Halabi/halabi_metadata.csv'
 df_annot = pd.read_csv(metadata)
 
 trypsin = df_annot[df_annot['Protein_type'] == 'Trypsin'].Seq_ID
@@ -35,40 +34,12 @@ _, chymotrypsin_xcor_id, chymotrypsin_xcor_seq = filter_seq_id(sector_seq,
 
 idmat_trypsin_sequences = compute_seq_identity(trypsin_seq)
 idmat_trypsin_xcor = compute_seq_identity(trypsin_xcor_seq)
+np.median(idmat_trypsin_xcor)
+# np.float64(0.6086956521739131)
 idmat_chymotrypsin_sequences = compute_seq_identity(chymotrypsin_seq)
 idmat_chymotrypsin_xcor = compute_seq_identity(chymotrypsin_xcor_seq)
-
-# Scatter plot
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), tight_layout=True)
-ax = axes[0]
-ax.plot(idmat_trypsin_sequences, idmat_trypsin_xcor, marker='o', linewidth=0,
-        markersize=4, zorder=50, color='#ff0000', alpha=0.2)
-ax.plot([0, 1], [0, 1], transform=ax.transAxes, color='black', ls='dashed')
-ax.tick_params(axis='both', which='both', labelsize='xx-small',
-               bottom=True, top=False, labeltop=False, labelbottom=True,
-               left=True, right=False, labelleft=True, labelright=False)
-ax.spines[['right', 'top']].set_visible(False)
-ax.set_xlim(0.5, 1)
-ax.set_ylim(0, 1)
-ax.set_xlabel('Full sequence', fontsize='x-small', fontweight='bold', labelpad=2)
-ax.set_ylabel('XCoR sequence', fontsize='x-small', fontweight='bold', labelpad=2)
-add_letter_and_title(ax, 'A.', 'Trypsin')
-
-ax = axes[1]
-ax.plot(idmat_chymotrypsin_sequences, idmat_chymotrypsin_xcor, marker='o',
-        linewidth=0, markersize=4, zorder=50, color='#00a08a', alpha=0.2)
-ax.plot([0, 1], [0, 1], transform=ax.transAxes, color='black', ls='dashed')
-ax.tick_params(axis='both', which='both', labelsize='xx-small',
-               bottom=True, top=False, labeltop=False, labelbottom=True,
-               left=True, right=False, labelleft=True, labelright=False)
-ax.spines[['right', 'top']].set_visible(False)
-ax.set_xlim(0.5, 1)
-ax.set_ylim(0, 1)
-ax.set_xlabel('Full sequence', fontsize='x-small', fontweight='bold', labelpad=2)
-ax.set_ylabel('XCoR sequence', fontsize='x-small', fontweight='bold', labelpad=2)
-add_letter_and_title(ax, 'B.', 'Chymotrypsin')
-fig.savefig('results/cocoatree_gt/halabi/trypsin_vs_chymotrypsin_pairwise_seqid_xcor_1_scatter.pdf')
-fig.savefig('results/cocoatree_gt/halabi/trypsin_vs_chymotrypsin_pairwise_seqid_xcor_1_scatter.png')
+np.median(idmat_chymotrypsin_xcor)
+# np.float64(0.30434782608695654)
 
 # Keep only lower triangular identity matrix as array
 tril_trypsin_xcor = idmat_trypsin_xcor[np.tril_indices(
@@ -81,22 +52,9 @@ tril_chymotrypsin_seq = idmat_chymotrypsin_sequences[np.tril_indices(
     idmat_chymotrypsin_sequences.shape[0])]
 
 # Distributions
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), tight_layout=True)
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 3), tight_layout=True)
 ax = axes[0]
 bins = np.arange(0, 1, 0.05)
-ax.hist(tril_trypsin_seq, bins=bins, alpha=0.7, label='Trypsins',
-        color='#ff0000', density=True)
-ax.hist(tril_chymotrypsin_seq, bins=bins, alpha=0.7, label='Chymotrypsins',
-        color='#00a08a', density=True)
-ax.legend(loc='upper left')
-ax.tick_params(axis='both', which='both', labelsize='xx-small',
-               bottom=True, top=False, labeltop=False, labelbottom=True,
-               left=True, right=False, labelleft=True, labelright=False)
-ax.set_xlabel('Pairwise sequence identity', fontsize='x-small',
-              fontweight='bold', labelpad=2)
-add_letter_and_title(ax, 'A.', 'Full sequences')
-
-ax = axes[1]
 ax.hist(tril_trypsin_xcor, bins=bins, alpha=0.7, label='Trypsins',
         color='#ff0000', density=True)
 ax.hist(tril_chymotrypsin_xcor, bins=bins, alpha=0.7, label='Chymotrypsins',
@@ -107,7 +65,153 @@ ax.tick_params(axis='both', which='both', labelsize='xx-small',
                left=True, right=False, labelleft=True, labelright=False)
 ax.set_xlabel('Pairwise sequence identity', fontsize='x-small',
               fontweight='bold', labelpad=2)
-add_letter_and_title(ax, 'B.', 'XCoR sequences')
-fig.savefig('results/cocoatree_gt/halabi/trypsin_vs_chymotrypsin_pairwise_seqid_xcor_1_hist.pdf')
-fig.savefig('results/cocoatree_gt/halabi/trypsin_vs_chymotrypsin_pairwise_seqid_xcor_1_hist.png')
-fig.savefig('results/cocoatree_gt/halabi/trypsin_vs_chymotrypsin_pairwise_seqid_xcor_1_hist.svg')
+add_letter_and_title(ax, 'A.', 'XCoR sequences')
+
+ax = axes[1]
+ax.hist(tril_trypsin_seq, bins=bins, alpha=0.7, label='Trypsins',
+        color='#ff0000', density=True)
+ax.hist(tril_chymotrypsin_seq, bins=bins, alpha=0.7, label='Chymotrypsins',
+        color='#00a08a', density=True)
+ax.legend(loc='upper left')
+ax.tick_params(axis='both', which='both', labelsize='xx-small',
+               bottom=True, top=False, labeltop=False, labelbottom=True,
+               left=True, right=False, labelleft=True, labelright=False)
+ax.set_xlabel('Pairwise sequence identity', fontsize='x-small',
+              fontweight='bold', labelpad=2)
+add_letter_and_title(ax, 'B.', 'Full sequences')
+fig.savefig('figures/halabi/trypsin_vs_chymotrypsin_pw_seqid_xcor_1_hist.pdf')
+fig.savefig('figures/halabi/trypsin_vs_chymotrypsin_pw_seqid_xcor_1_hist.svg')
+
+# Same thing but with vertebrate vs invertebrate in Green XCoR
+sector_file = 'results/cocoatree_gt/halabi/cocoatree_xcor_2_SCA_none.fasta'
+sector = load_MSA(sector_file, 'fasta')
+sector_seq = sector['alignment']
+sector_id = sector['sequence_ids']
+
+vertebrate = df_annot[df_annot['Subphylum'] == 'vertebrate'].Seq_ID
+vertebrate = list(map(str, vertebrate))
+invertebrate = df_annot[df_annot['Subphylum'] == 'invertebrate'].Seq_ID
+invertebrate = list(map(str, invertebrate))
+
+_, vertebrate_seq_id, vertebrate_seq = filter_seq_id(halabi_seq, halabi_id,
+                                                     vertebrate)
+_, vertebrate_xcor_id, vertebrate_xcor_seq = filter_seq_id(sector_seq,
+                                                           sector_id,
+                                                           vertebrate)
+_, invertebrate_seq_id, invertebrate_seq = filter_seq_id(halabi_seq, halabi_id,
+                                                         invertebrate)
+_, invertebrate_xcor_id, invertebrate_xcor_seq = filter_seq_id(sector_seq,
+                                                               sector_id,
+                                                               invertebrate)
+
+idmat_vertebrate_sequences = compute_seq_identity(trypsin_seq)
+idmat_vertebrate_xcor = compute_seq_identity(trypsin_xcor_seq)
+idmat_invertebrate_sequences = compute_seq_identity(invertebrate_seq)
+idmat_invertebrate_xcor = compute_seq_identity(invertebrate_xcor_seq)
+
+# Keep only lower triangular identity matrix as array
+tril_vertebrate_xcor = idmat_vertebrate_xcor[np.tril_indices(
+    idmat_vertebrate_xcor.shape[0])]
+tril_vertebrate_seq = idmat_vertebrate_sequences[
+    np.tril_indices(idmat_vertebrate_sequences.shape[0])]
+tril_invertebrate_xcor = idmat_invertebrate_xcor[np.tril_indices(
+    idmat_invertebrate_xcor.shape[0])]
+tril_invertebrate_seq = idmat_invertebrate_sequences[np.tril_indices(
+    idmat_invertebrate_sequences.shape[0])]
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 3), tight_layout=True)
+ax = axes[0]
+bins = np.arange(0, 1, 0.05)
+ax.hist(tril_vertebrate_xcor, bins=bins, alpha=0.7, label='Vertebrates',
+        color='#798e87', density=True)
+ax.hist(tril_invertebrate_xcor, bins=bins, alpha=0.7, label='Not vertebrates',
+        color='#c27d38', density=True)
+ax.legend(loc='upper right')
+ax.tick_params(axis='both', which='both', labelsize='xx-small',
+               bottom=True, top=False, labeltop=False, labelbottom=True,
+               left=True, right=False, labelleft=True, labelright=False)
+ax.set_xlabel('Pairwise sequence identity', fontsize='x-small',
+              fontweight='bold', labelpad=2)
+add_letter_and_title(ax, 'A.', 'XCoR sequences')
+
+ax = axes[1]
+ax.hist(tril_vertebrate_seq, bins=bins, alpha=0.7, label='Vertebrates',
+        color='#798e87', density=True)
+ax.hist(tril_invertebrate_seq, bins=bins, alpha=0.7, label='Not vertebrates',
+        color='#c27d38', density=True)
+ax.legend(loc='upper left')
+ax.tick_params(axis='both', which='both', labelsize='xx-small',
+               bottom=True, top=False, labeltop=False, labelbottom=True,
+               left=True, right=False, labelleft=True, labelright=False)
+ax.set_xlabel('Pairwise sequence identity', fontsize='x-small',
+              fontweight='bold', labelpad=2)
+add_letter_and_title(ax, 'B.', 'Full-length sequences')
+fig.savefig('figures/halabi/vertebrate_vs_invert_pw_seqid_xcor_2_hist.pdf')
+fig.savefig('figures/halabi/vertebrate_vs_invert_pw_seqid_xcor_2_hist.svg')
+
+# Same thing but with vertebrate vs invertebrate in Blue XCoR
+sector_file = 'results/cocoatree_gt/halabi/cocoatree_xcor_3_SCA_none.fasta'
+sector = load_MSA(sector_file, 'fasta')
+sector_seq = sector['alignment']
+sector_id = sector['sequence_ids']
+
+vertebrate = df_annot[df_annot['Subphylum'] == 'vertebrate'].Seq_ID
+vertebrate = list(map(str, vertebrate))
+invertebrate = df_annot[df_annot['Subphylum'] == 'invertebrate'].Seq_ID
+invertebrate = list(map(str, invertebrate))
+
+_, vertebrate_seq_id, vertebrate_seq = filter_seq_id(halabi_seq, halabi_id,
+                                                     vertebrate)
+_, vertebrate_xcor_id, vertebrate_xcor_seq = filter_seq_id(sector_seq,
+                                                           sector_id,
+                                                           vertebrate)
+_, invertebrate_seq_id, invertebrate_seq = filter_seq_id(halabi_seq, halabi_id,
+                                                         invertebrate)
+_, invertebrate_xcor_id, invertebrate_xcor_seq = filter_seq_id(sector_seq,
+                                                               sector_id,
+                                                               invertebrate)
+
+idmat_vertebrate_sequences = compute_seq_identity(trypsin_seq)
+idmat_vertebrate_xcor = compute_seq_identity(trypsin_xcor_seq)
+idmat_invertebrate_sequences = compute_seq_identity(invertebrate_seq)
+idmat_invertebrate_xcor = compute_seq_identity(invertebrate_xcor_seq)
+
+# Keep only lower triangular identity matrix as array
+tril_vertebrate_xcor = idmat_vertebrate_xcor[np.tril_indices(
+    idmat_vertebrate_xcor.shape[0])]
+tril_vertebrate_seq = idmat_vertebrate_sequences[
+    np.tril_indices(idmat_vertebrate_sequences.shape[0])]
+tril_invertebrate_xcor = idmat_invertebrate_xcor[np.tril_indices(
+    idmat_invertebrate_xcor.shape[0])]
+tril_invertebrate_seq = idmat_invertebrate_sequences[np.tril_indices(
+    idmat_invertebrate_sequences.shape[0])]
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), tight_layout=True)
+ax = axes[0]
+bins = np.arange(0, 1, 0.05)
+ax.hist(tril_vertebrate_xcor, bins=bins, alpha=0.7, label='Vertebrates',
+        color='#798e87', density=True)
+ax.hist(tril_invertebrate_xcor, bins=bins, alpha=0.7, label='Not vertebrates',
+        color='#c27d38', density=True)
+ax.legend(loc='upper right')
+ax.tick_params(axis='both', which='both', labelsize='xx-small',
+               bottom=True, top=False, labeltop=False, labelbottom=True,
+               left=True, right=False, labelleft=True, labelright=False)
+ax.set_xlabel('Pairwise sequence identity', fontsize='x-small',
+              fontweight='bold', labelpad=2)
+add_letter_and_title(ax, 'A.', 'XCoR sequences')
+
+ax = axes[1]
+ax.hist(tril_vertebrate_seq, bins=bins, alpha=0.7, label='Vertebrates',
+        color='#798e87', density=True)
+ax.hist(tril_invertebrate_seq, bins=bins, alpha=0.7, label='Not vertebrates',
+        color='#c27d38', density=True)
+ax.legend(loc='upper left')
+ax.tick_params(axis='both', which='both', labelsize='xx-small',
+               bottom=True, top=False, labeltop=False, labelbottom=True,
+               left=True, right=False, labelleft=True, labelright=False)
+ax.set_xlabel('Pairwise sequence identity', fontsize='x-small',
+              fontweight='bold', labelpad=2)
+add_letter_and_title(ax, 'B.', 'Full-length sequences')
+fig.savefig('figures/halabi/vertebrate_vs_invert_pw_seqid_xcor_3_hist.pdf')
+fig.savefig('figures/halabi/vertebrate_vs_invert_pw_seqid_xcor_3_hist.svg')
